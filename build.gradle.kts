9d32412bfb8393deb7edf8e368665adb37f74eb8
dcracauer <dave.cracauer@collaboration.ai>
2025-04-10 09:47:07 -0500

    Add initial Spring Boot Kotlin project setup

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+	alias(libs.plugins.kotlin.jvm)
+	alias(libs.plugins.kotlin.spring)
+	alias(libs.plugins.spring.boot)
+	alias(libs.plugins.spring.dependency.management)
+}
+
+group = "ai.collaboration"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation(libs.spring.boot.actuator)
+	implementation(libs.spring.boot.web)
+	implementation(libs.jackson.kotlin)
+	implementation(libs.kotlin.reflect)
+	runtimeOnly(libs.micrometer.otlp)
+	testImplementation(libs.spring.boot.test)
+	testImplementation(libs.kotlin.test.junit5)
+	testRuntimeOnly(libs.junit.platform.launcher)
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
