0073ca9895e19f6340c9aee62ab575164e4d8e23
dcracauer <dave.cracauer@collaboration.ai>
2025-04-10 13:44:24 -0500

    Introduce custom Gradle plugins for versioning and publishing
    Added `VersioningPlugin`, `PublishingConfigPlugin`, and `SetupPlugin` to streamline project configuration. Replaced inline configuration in `build.gradle.kts` with plugin-based setups and upgraded Axion Release Plugin to 1.18.18.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,60 +1,40 @@
 plugins {
-	alias(libs.plugins.kotlin.jvm)
-	alias(libs.plugins.kotlin.spring)
-	alias(libs.plugins.spring.boot)
-	alias(libs.plugins.spring.dependency.management)
-	alias(libs.plugins.axion)
-}
-
-project.afterEvaluate {
-	println("Version: ${project.version}")
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    id("project-setup")
 }
 
 group = "ai.collaboration"
 
-// Configure axion-release plugin
-scmVersion {
-	tag { prefix = "v" }
-	// Use the last tag as the version
-	useHighestVersion.set(true)
-	// Create a snapshot version if the code has uncommitted changes
-	snapshotCreator { version, _ ->
-		"$version-SNAPSHOT"
-	}
-	// Configure how the next version is calculated when running the release task
-	versionIncrementer("incrementPatch")
-	// Ignore uncommitted changes when checking if the repository is clean
-	ignoreUncommittedChanges.set(false)
-}
-
-
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation(libs.spring.boot.actuator)
-	implementation(libs.spring.boot.web)
-	implementation(libs.jackson.kotlin)
-	implementation(libs.kotlin.reflect)
-	runtimeOnly(libs.micrometer.otlp)
-	testImplementation(libs.spring.boot.test)
-	testImplementation(libs.kotlin.test.junit5)
-	testRuntimeOnly(libs.junit.platform.launcher)
+    implementation(libs.spring.boot.actuator)
+    implementation(libs.spring.boot.web)
+    implementation(libs.jackson.kotlin)
+    implementation(libs.kotlin.reflect)
+    runtimeOnly(libs.micrometer.otlp)
+    testImplementation(libs.spring.boot.test)
+    testImplementation(libs.kotlin.test.junit5)
+    testRuntimeOnly(libs.junit.platform.launcher)
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
